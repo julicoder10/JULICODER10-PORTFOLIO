5184f26baeea7da78b0633778da9baf652998f3e
julicoder10 <julicoder10@gmail.com>
2022-01-18 18:57:05 -0300

    Some css code, and animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PrincipalSectionComponent } from './components/principal-section/principal-section.component';
+import { SecundarySectionComponent } from './components/secundary-section/secundary-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    PrincipalSectionComponent,
+    SecundarySectionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/principal-section/principal-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-principal-section',
+  templateUrl: './principal-section.component.html',
+  styleUrls: ['./principal-section.component.scss']
+})
+export class PrincipalSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
